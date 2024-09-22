238d0ea2cdc31a9e894224954dbae6cf5c9a03a6
Arya-Kishan <arya12345kishan@gmail.com>
2024-09-22 13:30:34 +0530

    lazy_import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import HomePage from './pages/HomePage'
+import React, { lazy, Suspense } from 'react'
+import Navbar from './components/Landing/Navbar'
+const Front = lazy(() => import("./components/Landing/Front"))
+const Features = lazy(() => import("./components/Landing/Features"))
+const HowItWorks = lazy(() => import("./components/Landing/HowItWorks"))
+const Connect = lazy(() => import("./components/Landing/Connect"))
+const SocialProof = lazy(() => import("./components/Landing/SocialProof"))
+const Financials = lazy(() => import("./components/Landing/Financials"))
+const Footer = lazy(() => import("./components/Landing/Footer"))
+
 
 const App = () => {
   return (
     <div className='w-full min-h-dvh'>
-      <HomePage />
+      <Suspense fallback={"loading..."}>
+        <Navbar />
+        <Front />
+        <Features />
+        <HowItWorks />
+        <Connect />
+        <SocialProof />
+        <Financials />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
